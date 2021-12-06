706c4ba54ef2e17e0be148bbff7f0076a790f45c
Chidori <business.faissal@outlook.com>
2021-12-06 15:24:49 -0600

    refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,37 +3,31 @@ console.log(today);
 $("#currentDay").text(today);
 
 $(document).ready(function() {
-  $(".saveBtn").on("click", function() {
-    let time = $(this)
+  $(".saveBtn").click(function(event) {
+    event.preventDefault();
+    let hour = $(this)
                 .parent()
                 .attr("id");
-    let text = $(this)
+    let events = $(this)
                 .siblings(".description")
-                .val()
-                .trim();
-    console.log(this);
+                .val();
     // place it within the LS
-    localStorage.setItem(time, text);
-  })
-  $(".description #0900-h").val(localStorage.getItem("0900-h"));
-  $(".description #1000-h").val(localStorage.getItem("1000-h"));
-  $(".description #1100-h").val(localStorage.getItem("1100-h"));
-  $(".description #1200-h").val(localStorage.getItem("1200-h"));
-  $(".description #1300-h").val(localStorage.getItem("1300-h"));
-  $(".description #1400-h").val(localStorage.getItem("1400-h"));
-  $(".description #1500-h").val(localStorage.getItem("1500-h"));
-  $(".description #1600-h").val(localStorage.getItem("1600-h"));
-  $(".description #1700-h").val(localStorage.getItem("1700-h"));
+    localStorage.setItem(hour, events);
+  });
+
 
   let timeOfEvent = () => {
+    let currentTime = dayjs().format("h:mm A");
+    console.log(currentTime);
+
     $(".time-block").each(function () {
-      let blockHour = parseInt($(this).attr("id").split("hour")[1]);
-      // validate work
-      if (blockHour < today) {
+      let blockHour = parseInt($(this).attr("id").split("hour")[0]);
+      // validate time to determine which color will be displayed
+      if (blockHour < currentTime) {
         $(this).addClass("past");
         $(this).removeClass("present");
         $(this).removeClass("future");
-      } else if (blockHour === today) {
+      } else if (blockHour === currentTime) {
         $(this).removeClass("past");
         $(this).addClass("present");
         $(this).removeClass("future");
@@ -44,6 +38,16 @@ $(document).ready(function() {
       }
     });
   }
+
+  $("#9").val(localStorage.getItem("0900-h"));
+  $("#10").val(localStorage.getItem("1000-h"));
+  $("#11").val(localStorage.getItem("1100-h"));
+  $("#12").val(localStorage.getItem("1200-h"));
+  $("#13").val(localStorage.getItem("1300-h"));
+  $("#14").val(localStorage.getItem("1400-h"));
+  $("#15").val(localStorage.getItem("1500-h"));
+  $("#16").val(localStorage.getItem("1600-h"));
+  $("#17").val(localStorage.getItem("1700-h"));
   
   timeOfEvent();
 });
