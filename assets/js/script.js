222d0e22969387e88a05a14d510679b785e88224
Chidori <business.faissal@outlook.com>
2021-12-06 01:24:38 -0600

    js cleaned up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,39 @@
-//- WHEN I open the planner
-//- THEN the current day is displayed at the top of the calendar
 const today = dayjs().format("MMMM D, YYYY h:mm A");
 console.log(today);
 $("#currentDay").text(today);
 
 $(document).ready(function() {
-//- WHEN I scroll down
-//- THEN I am presented with time blocks for standard business hours
   $(".saveBtn").on("click", function() {
-    let time = $(this).parent().attr("id");
-    let text = $(this).siblings(".description").val();
+    let time = $(this)
+                .parent()
+                .attr("id");
+    let text = $(this)
+                .siblings(".description")
+                .val()
+                .trim();
+    console.log(this);
     // place it within the LS
     localStorage.setItem(time, text);
   })
+  $(".description #0900-h").val(localStorage.getItem("0900-h"));
+  $(".description #1000-h").val(localStorage.getItem("1000-h"));
+  $(".description #1100-h").val(localStorage.getItem("1100-h"));
+  $(".description #1200-h").val(localStorage.getItem("1200-h"));
+  $(".description #1300-h").val(localStorage.getItem("1300-h"));
+  $(".description #1400-h").val(localStorage.getItem("1400-h"));
+  $(".description #1500-h").val(localStorage.getItem("1500-h"));
+  $(".description #1600-h").val(localStorage.getItem("1600-h"));
+  $(".description #1700-h").val(localStorage.getItem("1700-h"));
 
   let timeOfEvent = () => {
-    let currentTime = dayjs().format("h:mm A");
-    console.log(currentTime);
-
     $(".time-block").each(function () {
       let blockHour = parseInt($(this).attr("id").split("hour")[1]);
       // validate work
-      if (blockHour < currentTime) {
+      if (blockHour < today) {
         $(this).addClass("past");
         $(this).removeClass("present");
         $(this).removeClass("future");
-      } else if (blockHour === currentTime) {
+      } else if (blockHour === today) {
         $(this).removeClass("past");
         $(this).addClass("present");
         $(this).removeClass("future");
@@ -36,19 +44,6 @@ $(document).ready(function() {
       }
     });
   }
-  //- WHEN I click the save button for that time block
-  //- THEN the text for that event is saved in local storage
   
   timeOfEvent();
-  //- WHEN I refresh the page
-  //- THEN the saved events persist
-  $(".description #0900-h").val(localStorage.getItem("0900-h"));
-  $(".description #1000-h").val(localStorage.getItem("1000-h"));
-  $(".description #1100-h").val(localStorage.getItem("1100-h"));
-  $(".description #1200-h").val(localStorage.getItem("1200-h"));
-  $(".description #1300-h").val(localStorage.getItem("1300-h"));
-  $(".description #1400-h").val(localStorage.getItem("1400-h"));
-  $(".description #1500-h").val(localStorage.getItem("1500-h"));
-  $(".description #1600-h").val(localStorage.getItem("1600-h"));
-  $(".description #1700-h").val(localStorage.getItem("1700-h"));
 });
